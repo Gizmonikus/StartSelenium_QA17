17aec5e54ee725fbd80a9d44874043a34bb45969
Gizmonikus <fleshlerd91@gmail.com>
2022-11-16 10:10:01 +0200

    StartSelenium

---
+++ src/main/java/com/telran/Main.java
@@ -0,0 +1,12 @@
+package com.telran;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+       int a = 5;
+       int b = 7;
+       for(;;){
+           
+       }
+    }
+}

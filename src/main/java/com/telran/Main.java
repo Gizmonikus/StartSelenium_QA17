6b81e0efbfc5f7d755133efbe664caa120fbb2fe
Gizmonikus <fleshlerd91@gmail.com>
2022-11-16 12:03:29 +0200

    classwork

--- src/main/java/com/telran/Main.java
+++ src/main/java/com/telran/Main.java
@@ -3,10 +3,5 @@ package com.telran;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-       int a = 5;
-       int b = 7;
-       for(;;){
-           
-       }
     }
 }
